5792638249c4b97e3334b0a478ea91ce6d8222c4
Harshit jaiswal <harshitjaiswal2005@gmail.com>
2024-01-05 01:13:26 +0530

    initial change

---
+++ todoScript.js
@@ -0,0 +1,45 @@
+
+let task = document.getElementById("task");
+let taskList = document.querySelector(".task-list");
+
+function addTask() {
+    let newTask = task.value;
+    if (newTask != "") {
+        let li = document.createElement("li");
+        li.innerHTML = newTask;
+        let span = document.createElement("span");
+        span.innerHTML = "❌";
+        li.appendChild(span);
+        taskList.appendChild(li);
+        console.log(newTask);
+    } else {
+        alert("Enter a task: ");
+    }
+    saveData();
+    task.value = "";
+}
+taskList.addEventListener(
+    "click",
+    function (event) {
+        if (event.target.tagName == "LI") {
+            event.target.classList.toggle("checked");
+            saveData();
+            console.log("clicked on li tag");
+        } else if (event.target.tagName == "SPAN") {
+            //event.target
+            saveData();
+            console.log("clicked on cross");
+        }
+    },
+    false
+);
+
+function saveData() {
+    localStorage.setItem("data", taskList.innerHTML);
+}
+
+function fetchData() {
+    localStorage.getItem("data");
+}
+
+fetchData();
